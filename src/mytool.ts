94e9a296bf0a5dfece75e8b5b55527697f77e41f
Emily Marigold Klassen <forivall@gmail.com>
2023-09-17 17:14:23 -0700

    format

--- src/mytool.ts
+++ src/mytool.ts
@@ -1,21 +1,25 @@
-import {BlockTool} from '@editorjs/editorjs';
-import {BlockToolConstructable, BlockToolConstructorOptions} from '@editorjs/editorjs/types/tools';
+import { BlockTool } from '@editorjs/editorjs';
+import {
+  BlockToolConstructable,
+  BlockToolConstructorOptions,
+} from '@editorjs/editorjs/types/tools';
 
 interface MyData {
-  foo?: string
+  foo?: string;
 }
 
 interface MyConfig {
   dingus?: boolean;
 }
 
-interface MyToolConstructorOptions extends Partial<BlockToolConstructorOptions<MyData, MyConfig>> {
+interface MyToolConstructorOptions
+  extends Partial<BlockToolConstructorOptions<MyData, MyConfig>> {
   api: BlockToolConstructorOptions['api'];
 }
 
 export class MyTool implements BlockTool {
   constructor(config: MyToolConstructorOptions) {
-    console.log('constructs', options)
+    console.log('constructs', options);
   }
 
   save(block: HTMLElement) {
@@ -24,10 +28,10 @@ export class MyTool implements BlockTool {
   render(): HTMLElement {
     console.log('Method render not implemented.');
     const el = document.createElement('div');
-    el.innerText = 'dongus'
+    el.innerText = 'dongus';
     return el;
   }
-} 
+}
 
 if (false) {
   const dummy: BlockToolConstructable = MyTool;
